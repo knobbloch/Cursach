c16372b8d3e25ec24cbe4f4ece48ac63eda21f91
Michael <68405115+Mishania12let@users.noreply.github.com>
2022-02-27 17:51:08 +0500

    тестим создание проекта в гите

---
+++ TestGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
