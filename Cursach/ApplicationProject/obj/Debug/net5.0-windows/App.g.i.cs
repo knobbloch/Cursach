a8e4c1e511ae458f3aed9c6160e819d30c0f5562
16Shadows <54994658+16Shadows@users.noreply.github.com>
2022-06-12 19:18:55 +0500

    Revert "16 shadows"

--- Cursach/ApplicationProject/obj/Debug/net5.0-windows/App.g.i.cs
+++ Cursach/ApplicationProject/obj/Debug/net5.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace ApplicationProject {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public static void Main() {
             ApplicationProject.App app = new ApplicationProject.App();
             app.Run();
