4f5e67fee45ac132b2d84562e36fa8fbe65ca8e5
16Shadows <54994658+16Shadows@users.noreply.github.com>
2022-06-22 12:14:17 +0500

    Revert "16 shadows"

---
+++ WpfMishaLibrary/DbEntities/PlanIncome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMishaLibrary.VisibleEntities;
+
+namespace WpfMishaLibrary.DbEntities
+{
+    public class PlanIncome: PlanIncomeVisible
+    {
+        public int PlanIncomeId { get; set; }
+        // Goes to the db as the number of Ticks (int)!!!
+        public long BeginDate { get; set; }
+        public long EndDate { get; set; }
+    }
+}
